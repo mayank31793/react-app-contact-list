d9e2a6534e93a737e828dbb519d8dc49052a7f7f
Mayank Pant <mayankpant317@gmail.com>
2025-02-06 04:50:32 +0530

    logo added along with styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAddressCard } from "@fortawesome/free-solid-svg-icons";
+
 function Header() {
   return (
     <>
       <div className="flex justify-between bg-[#c1cad6] p-4 text-amber-950">
-        <div className="logo">
-          <h2>Contacts</h2>
+        <div className="logo flex items-center">
+          <FontAwesomeIcon
+            icon={faAddressCard}
+            className="text-4xl text-[#324376]"
+          />
+          <span className="px-3 text-4xl font-bold text-[#324376]">
+            Contacts
+          </span>
         </div>
         <div className="searchbar h-12 w-80 rounded-4xl bg-gray-200 border-b-gray-600 border-2 border-solid">
           <input
